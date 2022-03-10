e174d51a010ed2c710e402a47cf31cddf7f941ec
Omar Khaled <omar.khaled.albeltagy@gmail.com>
2022-03-10 20:59:49 +0200

    Create AccountMedicalVisitProductModel.cs

---
+++ AMEKSA/AccountMedicalVisitModels/AccountMedicalVisitProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMEKSA.AccountMedicalVisitModels
+{
+    public class AccountMedicalVisitProductModel
+    {
+        public int ProductId { get; set; }
+    }
+}
